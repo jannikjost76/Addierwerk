2d022af45d90cebf34ad0c819b092695300c08f7
Jannik Jost <jannik-jost@gmx.de>
2018-11-11 20:49:02 +0100

    client cleanup

---
+++ client/src/renderer/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <div class="left-side">
+      </div>
+
+      <div class="right-side">
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  methods: {}
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Source Sans Pro", sans-serif;
+}
+
+#wrapper {
+  background: radial-gradient(
+    ellipse at top left,
+    rgba(255, 255, 255, 1) 40%,
+    rgba(229, 229, 229, 0.9) 100%
+  );
+  height: 100vh;
+  padding: 60px 80px;
+  width: 100vw;
+}
+
+main {
+  display: flex;
+  justify-content: space-between;
+}
+
+main > div {
+  flex-basis: 50%;
+}
+
+.left-side {
+  display: flex;
+  flex-direction: column;
+}
+</style>
